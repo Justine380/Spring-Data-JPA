a10343f3b4eccb0295a9656caa9e070b6a7e8c78
Justine380 <justinenabaala@gmail.com>
2023-08-13 01:56:51 +0300

    Adding Sequences & Columns to table properties

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -1,15 +1,52 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name="Employee")
 public class Employee {
     @Id
+    @SequenceGenerator(
+            name = "employee_sequence",
+            sequenceName = "employee_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_sequence"
+    )
+
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String firstName;
+
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
     private String lastName;
+
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "TEXT",
+            unique = true
+
+    )
     private String email;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
     private Integer age;
 
     public Employee(Long id, String firstName, String lastName, String email, Integer age) {
@@ -18,6 +55,10 @@ public class Employee {
         this.lastName = lastName;
         this.email = email;
         this.age = age;
+    }
+
+    public Employee() {
+
     }
 
     public Long getId() {
